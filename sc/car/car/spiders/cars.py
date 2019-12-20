0a6f7e3e0669d2079d31ca3f9c0ca5946bcf77d1
sman01 <shreemun@gmail.com>
2019-12-20 23:31:24 +0530

    aye

--- sc/car/car/spiders/cars.py
+++ sc/car/car/spiders/cars.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-
+import nltk
+from textblob import TextBlob
 class CarsSpider(scrapy.Spider):
     name = 'cars'
     allowed_domains = ['https://auto.ndtv.com/reviews/tata-altroz-review-2145527']
@@ -9,6 +9,12 @@ class CarsSpider(scrapy.Spider):
 
     def parse(self, response):
         orders=response.xpath("//div[@class='article__content h__mb40']/p/text()").extract()
-        print (orders)
-        
+        order=" "
+#        blob=TextBlob(order)
+ #       print(format(blob.sentiment))
+       # print (order +"beep")
+        with open('analyse.txt', 'w') as f:
+            for item in orders:
+                f.write("%s\n" % item)
 
+       
