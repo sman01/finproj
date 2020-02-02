4039c7faec79d682fbb58c712943b8004a11ca42
sman01 <shreemun@gmail.com>
2020-02-02 21:38:23 +0530

    changes in t, web and web 2 and 1 file was removed

--- web.py
+++ web.py
@@ -1,31 +1,120 @@
-#Python program to scrape website 
-#and save quotes from website 
-import requests 
-from bs4 import BeautifulSoup 
-import csv 
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
 
-URL = "http://www.values.com/inspirational-quotes"
-r = requests.get(URL) 
+class TwitterClient(object):
+    '''
+    Generic Twitter Class for sentiment analysis.
+    '''
+    def __init__(self):
+        '''
+        Class constructor or initialization method.
+        '''
+        # keys and tokens from the Twitter Dev Console
+        consumer_key = 'vujZ19s53rDc8abpIDsrqavSV'
+        consumer_secret = 'JZaB1EgUSXEkzQ2gIE6pQEgbTCOYD1DB2426jwvXAYxWALY90F'
+        access_token = '2153140267-WDN6WMN3KQYpwWXCFQeNkbuyQod0tZgR2cZOU0P'
+        access_token_secret = 'lv4u2ynz6Ekn08Y9pIu0ohNPI0pZIvl1S98yyd26TjXfj'
 
-soup = BeautifulSoup(r.content, 'html5lib') 
+        # attempt authentication
+        try:
+            # create OAuthHandler object
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            # set access token and secret
+            self.auth.set_access_token(access_token, access_token_secret)
+            # create tweepy API object to fetch tweets
+            self.api = tweepy.API(self.auth)
+        except:
+            print("Error: Authentication Failed")
 
-quotes=[] # a list to store quotes 
+    def clean_tweet(self, tweet):
+        '''
+        Utility function to clean tweet text by removing links, special characters
+        using simple regex statements.
+        '''
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
-table = soup.find('div', attrs = {'id':'container'}) 
+    def get_tweet_sentiment(self, tweet):
+        '''
+        Utility function to classify sentiment of passed tweet
+        using textblob's sentiment method
+        '''
+        # create TextBlob object of passed tweet text
+        analysis = TextBlob(self.clean_tweet(tweet))
+        # set sentiment
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
 
-for row in table.findAll('div', attrs = {'class':'quote'}): 
-	quote = {} 
-	quote['theme'] = row.h5.text 
-	quote['url'] = row.a['href'] 
-	quote['img'] = row.img['src'] 
-	quote['lines'] = row.h6.text 
-	quote['author'] = row.p.text 
-	quotes.append(quote) 
+    def get_tweets(self, query, count = 10):
+        '''
+        Main function to fetch tweets and parse them.
+        '''
+        # empty list to store parsed tweets
+        tweets = []
 
-filename = 'inspirational_quotes.csv'
-with open(filename, 'wb') as f: 
-	w = csv.DictWriter(f,['theme','url','img','lines','author']) 
-	w.writeheader() 
-	for quote in quotes: 
-		w.writerow(quote) 
+        try:
+            # call twitter api to fetch tweets
+            fetched_tweets = self.api.search(q = query, count = count)
+
+            # parsing tweets one by one
+            for tweet in fetched_tweets:
+                # empty dictionary to store required params of a tweet
+                parsed_tweet = {}
+
+                # saving text of tweet
+                parsed_tweet['text'] = tweet.text
+                # saving sentiment of tweet
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+
+                # appending parsed tweet to tweets list
+                if tweet.retweet_count > 0:
+                    # if tweet has retweets, ensure that it is appended only once
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+
+            # return parsed tweets
+            return tweets
+
+        except tweepy.TweepError as e:
+            # print error (if any)
+            print("Error : " + str(e))
+
+def main():
+    # creating object of TwitterClient Class
+    api = TwitterClient()
+    # calling function to get tweets
+    tweets = api.get_tweets(query = 'Maruti Suzuki Alto', count = 2000)
+
+    # picking positive tweets from tweets
+    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+    # percentage of positive tweets
+    print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+    # picking negative tweets from tweets
+    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+    # percentage of negative tweets
+    print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+    # percentage of neutral tweets
+    print("Neutral tweets percentage: {} %".format(100*(len(tweets) - len(ntweets) - len(ptweets))/len(tweets)))
+
+    # printing first 5 positive tweets
+    print("\n\nPositive tweets:")
+    for tweet in ptweets[:10]:
+        print(tweet['text'])
+
+    # printing first 5 negative tweets
+    print("\n\nNegative tweets:")
+    for tweet in ntweets[:10]:
+        print(tweet['text'])
+
+if __name__ == "__main__":
+    # calling main function
+    main()
+
 
